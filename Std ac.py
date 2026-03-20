b170f16f3355a247c879b5be46a94bc61431e3cf
unknown <08250279.sce@rub.edu.bt>
2026-03-20 16:09:44 +0600

    clear and informative result

---
+++ Std ac.py
@@ -0,0 +1,19 @@
+print ("Hello")
+Name = (input("Enter your name: "))
+num1 = float(input("Enter first number"))
+num2 = float(input("Enter second number: "))
+additional_result = num1 + num2 
+subtractional_result = num1 - num2
+multiplication_result = num1 * num2
+division_result = num1 / num2
+print(Name)
+print(additional_result)
+print(subtractional_result)
+print(multiplication_result)
+print(division_result)
+print("Here are the clear result for your calculations")
+
+
+
+
+
