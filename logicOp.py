8bd6712f2ea98ed5fdab7cf804c9944a454e821c
unknown <08250279.sce@rub.edu.bt>
2026-03-20 15:43:33 +0600

    logical true or false

---
+++ logicOp.py
@@ -0,0 +1,11 @@
+x = True
+y = False
+
+result_and = x and y
+print("logical AND:", result_and)
+
+result_or = x or y
+print("logical OR:", result_or)
+
+result_not = not x
+print("Logical NOT:", result_not)
